deed3c63c291b9e0b56457044335a7909761da78
Faisal S <Faisal.Saleem@nfstechgroup.com>
2024-11-18 16:41:21 +0000

    LSP for csharp for linux and windows

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,5 +6,6 @@ return {
         require('bufferline').setup {}
         vim.keymap.set('n','<Tab>',':BufferLineCycleNext<CR>', {})
         vim.keymap.set('n','<S-Tab>',':BufferLineCyclePrev<CR>',{})
+        vim.keymap.set('n','<C-X>',':bd<CR>', {})
     end
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,7 +9,7 @@ return {
         'williamboman/mason-lspconfig.nvim',
         config = function()
             require('mason-lspconfig').setup({
-                ensure_installed = { 'lua_ls','csharp_ls','html','ts_ls' }
+                ensure_installed = { 'luau_lsp','omnisharp','html','ts_ls' }
             })
         end
     },
@@ -17,9 +17,24 @@ return {
         'neovim/nvim-lspconfig',
         config = function()
             local lspconfig = require('lspconfig')
+            local os_name = vim.loop.os_uname().sysname
+            local omnisharp_cmd = ""
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.csharp_ls.setup({})
+            if os_name == "Windows_NT" then
+                omnisharp_cmd = 'C:\\omnisharp\\omnisharp.exe' -- Path for Windows
+            else
+                omnisharp_cmd = '/mnt/c/omnisharp-linux-x64/omnisharp/run' -- Path for Linux
+            end
+            lspconfig.luau_lsp.setup({})
+            lspconfig.omnisharp.setup({
+                cmd = {
+                    omnisharp_cmd,
+                    '--languageserver',
+                    '--hostPID',
+                    tostring(vim.fn.getpid())
+                },
+                root_dir = lspconfig.util.root_pattern('*.sln', '*.csproj')
+            })
             lspconfig.html.setup({})
             lspconfig.ts_ls.setup({})
             
